0e39abaa5e91db0b37a0db8208ba367a4f99f2f0
Shivesh Bharti <shiveshbharti7252@gmail.com>
2023-01-13 17:19:49 +0530

    Added the filtere by year changes that means now you can filter your expenses list according to year selected

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -46,7 +46,7 @@ const ExpenseForm = (props) => {
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type='date' min="2023-01-01" max="2023-12-31" value={enteredDate} onChange={dateChangeHandler} />
+                <input type='date' min="2019-01-01" max="2023-12-31" value={enteredDate} onChange={dateChangeHandler} />
             </div>
         </div>
 
